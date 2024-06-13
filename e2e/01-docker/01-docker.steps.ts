4a08f6a0ff4a27a5cfdb885ce98c1eb293fb04a4
Khoi <lenguyenkhoi2806@gmail.com>
2024-06-13 17:16:30 +0700

    feat: resolve conflict

--- e2e/01-docker/01-docker.steps.ts
+++ e2e/01-docker/01-docker.steps.ts
@@ -1,10 +1,10 @@
-import { createBdd } from "playwright-bdd";
-import { APIRequestContext, APIResponse, expect } from "@playwright/test";
-import { test } from "../fixtures";
+import { createBdd } from 'playwright-bdd';
+import { APIRequestContext, APIResponse, expect } from '@playwright/test';
+import { test } from '../fixtures';
 
 const { Given, When, Then, After } = createBdd(test);
 
-let apiContext: APIRequestContext;
+let apiContext:APIRequestContext;
 let response: APIResponse;
 
 After(async () => {
@@ -12,30 +12,21 @@ After(async () => {
   response?.dispose();
 });
 
-Given(
-  'I started the Docker application with "docker run docker-app:latest -p {int}:80"',
-  async ({ playwright }, port: number) => {
-    apiContext = await playwright.request.newContext({
-      baseURL: `http://127.0.0.1:${port}`,
-    });
-  }
-);
+Given('I started the Docker application with "docker run docker-app:latest -p {int}:80"', async ({ playwright }, port:number) => {
+  apiContext = await playwright.request.newContext({
+    baseURL: `http://127.0.0.1:${port}`
+  });
+})
 
-When("I request to {string}", async ({}, endpoint: string) => {
+When('I request to {string}', async ({}, endpoint: string) => {
   response = await apiContext.get(endpoint);
 });
 
-Then(
-  "I should receive a response with status {int}",
-  async ({}, expectedStatus: number) => {
-    expect(response.status()).toBe(expectedStatus);
-  }
-);
+Then('I should receive a response with status {int}', async ({}, expectedStatus: number) => {
+  expect(response.status()).toBe(expectedStatus);
+});
 
-Then(
-  "I should receive a response with body {string}",
-  async ({}, expectedBody: string) => {
-    const actualBody = await response.text();
-    expect(actualBody).toBe(expectedBody);
-  }
-);
+Then('I should receive a response with body {string}', async ({}, expectedBody: string) => {
+  const actualBody = await response.text();
+  expect(actualBody).toBe(expectedBody);
+});
