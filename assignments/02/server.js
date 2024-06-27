6fed560b265a85a21be1c42f44a1809e5e5305be
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2024-06-27 16:38:42 +0700

    feat: add redis

--- assignments/02/server.js
+++ assignments/02/server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const generateUniqueId = require("generate-unique-id");
 const { Pool } = require("pg");
 const cors = require("cors");
+const redis = require("redis");
+const fetch = require("node-fetch");
+const { promisify } = require("util");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,10 +18,16 @@ const pool = new Pool({
   port: 5432,
 });
 
+const redisClient = redis.createClient({
+  host: "localhost",
+  port: 6379,
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(cors());
+redisClient.connect();
 
 app.post("/api/shorten", async (req, res) => {
   try {
@@ -41,6 +50,15 @@ app.post("/api/shorten", async (req, res) => {
         [originalUrl, `${req.headers.host}/${shortURL}`]
       );
       client.release();
+      const htmlContent = await fetch(originalUrl).then((response) =>
+        response.text()
+      );
+      redisClient.set(
+        `${req.headers.host}/${shortURL}`,
+        htmlContent,
+        "EX",
+        3600
+      );
       res
         .status(201)
         .json({ originalUrl, shortURL: result.rows[0].shortened_url });
@@ -62,12 +80,27 @@ app.get("/:shortURL", async (req, res) => {
     client.release();
     if (result.rows.length > 0) {
       const originalUrl = result.rows[0].original_url;
-      res.redirect(301, originalUrl);
+      const cachedHtml = await redisClient.get(
+        `${req.headers.host}/${shortURL}`
+      );
+      if (cachedHtml) {
+        res.send(cachedHtml);
+      } else {
+        const response = await fetch(originalUrl);
+        const htmlContent = await response.text();
+        redisClient.set(
+          `${req.headers.host}/${shortURL}`,
+          htmlContent,
+          "EX",
+          3600
+        );
+        res.send(htmlContent);
+      }
     } else {
       res.status(404).json({ error: "Short URL not found" });
     }
   } catch (error) {
-    console.error("Error executing query", error);
+    console.error("Error executing query or Redis operation", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
